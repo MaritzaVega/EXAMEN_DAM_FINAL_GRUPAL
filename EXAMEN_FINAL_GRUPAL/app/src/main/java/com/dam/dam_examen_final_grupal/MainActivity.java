a0ecf120250f4f076492ada54d8e00fe02fb12d0
Maritza Vega <2016200251@untels.edu.pe>
2021-02-11 14:39:57 -0500

    Maritza Vega Vergara

---
+++ EXAMEN_FINAL_GRUPAL/app/src/main/java/com/dam/dam_examen_final_grupal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dam.dam_examen_final_grupal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//repositorio y proyecto url : https://github.com/MaritzaVega/EXAMEN_DAM_FINAL_GRUPAL.git
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
