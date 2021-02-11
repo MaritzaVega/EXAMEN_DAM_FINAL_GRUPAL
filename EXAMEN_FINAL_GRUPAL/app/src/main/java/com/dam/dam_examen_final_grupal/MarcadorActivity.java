4da8094ff29042b743c540baab48a2181295feb4
Carla Lizarraga Vasquez <2016200129@untels.edu.pe>
2021-02-11 17:04:53 -0500

    Carla Lizarraga Vasquez

--- EXAMEN_FINAL_GRUPAL/app/src/main/java/com/dam/dam_examen_final_grupal/MarcadorActivity.java
+++ EXAMEN_FINAL_GRUPAL/app/src/main/java/com/dam/dam_examen_final_grupal/MarcadorActivity.java
@@ -3,6 +3,7 @@ package com.dam.dam_examen_final_grupal;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +15,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MarcadorActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Double latitud;
+    private Double longitud;
+    private String dato1, dato2, dato3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +27,31 @@ public class MarcadorActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+        //Obtenemos los datos
+        dato1 = getIntent().getStringExtra("dato1");
+        dato2 = getIntent().getStringExtra("dato2");
+        dato3 = getIntent().getStringExtra("dato3");
+
+        latitud = Double.parseDouble(dato1);
+        longitud = Double.parseDouble(dato2);
+
+        Toast.makeText(this,dato3 , Toast.LENGTH_SHORT).show();
+
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        final LatLng carla = new LatLng(latitud,longitud);
+
+        mMap.addMarker(new MarkerOptions().position(carla).title(dato3));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(carla));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
